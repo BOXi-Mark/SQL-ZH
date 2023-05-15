6c7484f9b1bb1377f6901d400f1c91c4d4a4299c
Dobozi Márk <117992644+BOXi-Mark@users.noreply.github.com>
2023-05-15 08:29:06 +0200

    Create ZH_minta.sql

---
+++ ZH_minta.sql
@@ -0,0 +1,37 @@
+-- Hot_dog tábla létrehozása
+CREATE TABLE Hot_dog (
+    Azon INT PRIMARY KEY,
+    Ar DECIMAL(5, 2) NOT NULL CHECK (Ar >= 1000)
+);
+
+-- Feltetek tábla létrehozása
+CREATE TABLE Feltetek (
+    Id INT PRIMARY KEY,
+    Feltet VARCHAR(20)
+);
+
+-- Hot_dog_feltetek tábla létrehozása a kapcsolatok tárolásához
+CREATE TABLE Hot_dog_feltetek (
+    Hot_dog_Azon INT,
+    Feltetek_Id INT,
+    PRIMARY KEY (Hot_dog_Azon, Feltetek_Id),
+    FOREIGN KEY (Hot_dog_Azon) REFERENCES Hot_dog(Azon),
+    FOREIGN KEY (Feltetek_Id) REFERENCES Feltetek(Id)
+);
+
+CREATE TABLE Megrendelo (
+    Azon INT PRIMARY KEY,
+    Nev VARCHAR(50),
+    Telefonszam VARCHAR(20)
+);
+
+CREATE TABLE Megrendeles (
+    MegrendeloAzon INT,
+    Hot_dogAzon INT,
+    MegrendeloDatum DATE,
+    KiszallitasDatum DATE,
+    FOREIGN KEY (MegrendeloAzon) REFERENCES Megrendelo(Azon),
+    FOREIGN KEY (Hot_dogAzon) REFERENCES Hot_dog(Azon),
+    CHECK (KiszallitasDatum > MegrendeloDatum),
+    PRIMARY KEY (MegrendeloAzon, Hot_dogAzon)
+);
